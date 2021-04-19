4bee0ca39f5e0e31de478268318b96a348be478f
Andreas Bohne-Lang <andreas.bohne-lang@medma.uni-heidelberg.de>
2021-04-19 14:47:44 +0200

    Signed-off-by: Andreas Bohne-Lang <andreas.bohne-lang@medma.uni-heidelberg.de>

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Pubmed Reference List by JavaScript
+
+This small JavaScript generates a reference list from a PubMed query directly.
+
+
+## For example: Search by author: "Bohne-Lang"[AU]
+```
+<div id="LitList"></div>
+<script type="application/javascript"> getEntries("LitList","(Medical Faculty Mannheim[AD])") </script>
+```
+
+![example](https://raw.githubusercontent.com/bohnelang/PubmedJavascriptReferenceList/main/example1.jpg)
+
+## Code
+The structure of the code is quite simple
+* Fetch PubMed online by a 'normal' query against the RESTful API of the eUtils 
+* Get a list of IDs in JSON format
+* Compute a new query for each ID
+* Get a record with details of this publication
+* Render this publication details in HTML and add a new entry on-the-fly to DOM
+
+## Other queries:
+See https://pubmed.ncbi.nlm.nih.gov/help/#search-tags for details.
+
+* by ID: 15982415,15784146[ID]
+* by Affiliation (Medical Faculty Mannheim[AD])
+![example](https://raw.githubusercontent.com/bohnelang/PubmedJavascriptReferenceList/main/example2.jpg)
---
+++ pubmedreferencelist.js
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<title>
+Demo Pubmed-ID + reference list by JavaScript
+</title>
+
+<script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script type="application/javascript">
+
+//
+// Javascript to build a reference list  by JavaScript and a PubMed query online
+// Andreas Bohne-Lang (2021)
+// Distributed by CC0 
+//
+
+
+function decodeEntities(encodedString) {
+    return $("<div/>").html(encodedString).text();
+}
+
+function filterTag(titleString) {
+    return titleString.replace("</li>", "").replace("<li>", "");
+}
+
+
+function getEntries(idbox,searchTerm) {
+
+    let element = document.getElementById(idbox);
+
+    // Query string for REST-API
+    let query_string1 = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + searchTerm + "&retmax=100&retmode=json";
+    //alert(query_string1);
+
+    var allPublications = document.createElement('ul');
+
+    allPublications.setAttribute("class", "pubList");
+
+    let idString = "";
+    let req = $.getJSON(query_string1, function(data) {
+        if (data != null) {
+            idString = data.esearchresult.idlist.join(",");
+        }
+    });
+
+
+    req.done(function(response) {
+
+        let query_string2 = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=" + idString + "&version=2.0&retmode=json";
+        //alert(query_string2);
+
+        $.getJSON(query_string2, function(data) {
+            if (data != null && data.result && data.result.uids) {
+
+                // Gets UIDs from JSON
+                let uids = data.result.uids;
+
+                // Reverse sorting so that the newest publications are on top of the list.
+                uids.sort().reverse();
+
+                $.each(uids, function(index, content) {
+                    let value = data.result[content];
+                    let authorListLength = value.authors.length;
+
+                    // Create list elemente
+                    let publicationEntry = document.createElement('li');
+
+                    // Iterate through list of authors
+                    for (i = 0; i < authorListLength - 1; i++) {
+                        publicationEntry.innerHTML += value.authors[i].name;
+                        if (i < authorListLength - 2) {
+                            publicationEntry.innerHTML += ", ";
+                        }
+                    }
+
+                    if (value.pubdate) {
+                        publicationEntry.innerHTML += " (" + parseInt(value.pubdate) + ")<br>";
+                    }
+
+                    if (value.title) {
+                        publicationEntry.innerHTML += decodeEntities(filterTag(value.title)) + "<br>";
+                    }
+
+                    if (value.fulljournalname) {
+                        publicationEntry.innerHTML += decodeEntities(value.fulljournalname);
+                    }
+
+                    if (value.volume) {
+                        publicationEntry.innerHTML += " " + value.volume;
+                    }
+
+                    if (value.issue) {
+                        publicationEntry.innerHTML += "(" + value.issue + ")";
+                    }
+
+                    if (value.pages) {
+                        publicationEntry.innerHTML += ": " + value.pages + "<br>";
+                    }
+
+                    $.each(value.articleids, function(index, value) {
+                        if (value.idtype == "doi") {
+                            publicationEntry.innerHTML += "<a href='http://dx.doi.org/" + value.value + "' target=_blank> DOI: " + value.value + "</a>  ";
+                        }
+                        if (value.idtype == "pubmed") {
+                            publicationEntry.innerHTML += "<a href='http://www.ncbi.nlm.nih.gov/pubmed/" + value.value + "' target=_blank> PMID: " + value.value + "</a> ";
+                            publicationEntry.innerHTML += "<a href='http://europepmc.org/abstract/MED/" + value.value + "' target=_blank> EuropePMC: " + value.value + "</a> ";
+                        }
+                    });
+                    allPublications.appendChild(publicationEntry);
+                });
+            }
+        });
+
+        element.appendChild(allPublications);
+
+    });
+
+};
+</script>
+
+<style>
+ .pubList { margin-left:35px;}  
+
+ div > ul > li > a { 
+	margin-right:5px;   
+	text-decoration:none;
+}
+	}
+</style>
+</head>
+<body>
+<div id=LitList></div>
+
+<script type="application/javascript"> getEntries("ListList","(Medical Faculty Mannheim[AD])") </script>
+</body>
+</html>
